1ee04a95f9944314d772488390d06e365623daf1
FurkanUcuncu <73463054+FurkanUcuncu@users.noreply.github.com>
2023-11-25 22:09:14 +0300

    Beta case completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Suspense, useEffect } from 'react';
+import './App.scss';
+import 'react-toastify/dist/ReactToastify.css';
+import { useAppDispatch } from './hooks/redux-hooks';
+import { CircularProgress } from '@mui/material';
+import ProductList from './pages/ProductList';
+import { getSessionId } from './store/auth/authSlice';
+import { viewCart } from './store/product/productSlice';
+import Layout from './components/Layout';
 
-function App() {
+const App = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getSessionId()).then(() => dispatch(viewCart()));
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<CircularProgress />}>
+      <Layout>
+        <ProductList />
+      </Layout>
+    </Suspense>
   );
-}
+};
 
 export default App;
